c3b0ca0369d4ae2ba2f80fdde7ff8be67e9621d4
MATarg81 <81872144+MATarg81@users.noreply.github.com>
2022-04-18 20:23:06 -0300

    Update homework.js

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -64,7 +64,7 @@ function sonIguales(x, y) {
   // Devuelve "true" si "x" e "y" son iguales
   // De lo contrario, devuelve "false"
   // Tu código:
-  if(x == y){
+  if(x === y){
     return true;
   }
 
@@ -75,7 +75,7 @@ function tienenMismaLongitud(str1, str2) {
   // Devuelve "true" si las dos strings tienen la misma longitud
   // De lo contrario, devuelve "false"
   // Tu código:
-  if(str1.length == str2.length){
+  if(str1.length === str2.length){
     return true;
   }
 
@@ -115,7 +115,7 @@ function esPar(num) {
   // Devuelve "true" si "num" es par
   // De lo contrario, devuelve "false"
   // Tu código:
-  if(num % 2 == 0){
+  if(num % 2 === 0){
     return true;
   }
 
@@ -126,7 +126,7 @@ function esImpar(num) {
   // Devuelve "true" si "num" es impar
   // De lo contrario, devuelve "false"
   // Tu código:
-  if(num % 2 == 0){
+  if(num % 2 === 0){
     return false;
   }
 
@@ -253,7 +253,7 @@ function esVocal(letra){
   // Si no es vocal, tambien debe devolver "Dato incorrecto".
   //Escribe tu código aquí
   var vocal = letra;
-  if(vocal == 'a' || vocal == 'e' || vocal == 'i' || vocal == 'o' || vocal == 'u' || vocal == 'A' || vocal == 'E' || vocal == 'I' || vocal == 'O' || vocal == 'U' && vocal.lenght == 1){
+  if(vocal === 'a' || vocal === 'e' || vocal === 'i' || vocal === 'o' || vocal === 'u' || vocal === 'A' || vocal === 'E' || vocal === 'I' || vocal === 'O' || vocal === 'U' && vocal.lenght === 1){
     return 'Es vocal';
 
   }return 'Dato incorrecto';
